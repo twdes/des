f65d3944e18fdf54ece13850b4b50e08d0f6873b
neolithos <neolithos@outlook.com>
2016-01-10 17:52:18 +0100

    Add: First Debug-Commands

--- SimpleDbg/DebugView.cs
+++ SimpleDbg/DebugView.cs
@@ -12,11 +12,52 @@ namespace TecWare.DE.Server
 	{
 		private object screenLock = new object();
 
+		public void WriteLine()
+		{
+			WriteLine(String.Empty);
+		} // proc WriteLine
+
 		public void WriteLine(string text)
 		{
 			lock (screenLock)
 				Console.Out.WriteLine(text);
 		} // proc WriteLine
+
+		public void WriteObject(object o)
+		{
+			if (o == null)
+				WriteLine("<null>");
+			else
+				WriteLine(o.ToString());
+		} // proc WriteObject
+
+		public void WriteError(Exception exception)
+		{
+			if (exception == null)
+				return;
+
+			var aggEx = exception as AggregateException;
+			if (aggEx == null)
+			{
+				// write exception
+				lock(screenLock)
+				{
+					using (SetColor(ConsoleColor.DarkRed))
+					{
+						Console.WriteLine($"[{exception.GetType().Name}]");
+						Console.WriteLine($"  {exception.Message}");
+          }
+				}
+
+				// chain exceptions
+				WriteError(exception.InnerException);
+			}
+			else
+			{
+				foreach (var ex in aggEx.InnerExceptions)
+					WriteError(ex);
+			}
+		} // proc WriteError
 
 		public void WriteError(string text)
 		{
@@ -24,24 +65,79 @@ namespace TecWare.DE.Server
 				Console.Error.WriteLine(text);
 		} // proc WriteError
 
+		public void Write(string text)
+			=> Write(text, ConsoleColor.Gray);
+
 		public void Write(string text, ConsoleColor foreground, ConsoleColor background = ConsoleColor.Black)
 		{
 			lock (screenLock)
 			{
-				var oldFg = Console.ForegroundColor;
-				var oldBg = Console.BackgroundColor;
-				try
-				{
-					Console.ForegroundColor = foreground;
-					Console.BackgroundColor = background;
+				using (SetColor(foreground, background))
 					Console.Write(text);
-				}
-				finally
-				{
-					Console.ForegroundColor = oldFg;
-					Console.BackgroundColor = oldBg;
-				}
 			}
 		} // proc WriteLine
+
+		#region -- SetColor ---------------------------------------------------------------
+
+		///////////////////////////////////////////////////////////////////////////////
+		/// <summary></summary>
+		private sealed class ResetColors : IDisposable
+		{
+			public ResetColors(ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+			{
+				this.OldBackgroundColor = Console.BackgroundColor;
+				this.OldForegroundColor = Console.ForegroundColor;
+				Console.BackgroundColor = backgroundColor;
+				Console.ForegroundColor = foregroundColor;
+			} // ctor
+
+			public void Dispose()
+			{
+				Console.ForegroundColor = this.OldForegroundColor;
+				Console.BackgroundColor = this.OldBackgroundColor;
+			} // proc Dispose
+
+			public ConsoleColor OldForegroundColor { get; }
+			public ConsoleColor OldBackgroundColor { get; }
+		} // class ResetColors
+
+		public IDisposable SetColor(ConsoleColor foreground, ConsoleColor background = ConsoleColor.Black)
+			=> new ResetColors(foreground, background);
+
+		#endregion
+
+		#region -- SetCursor --------------------------------------------------------------
+
+		private sealed class ResetCursor : IDisposable
+		{			
+			public ResetCursor(int left, int  top, bool visible)
+			{
+				this.OldCursorLeft = Console.CursorLeft;
+				this.OldCursorTop = Console.CursorTop;
+				this.OldVisible = Console.CursorVisible;
+
+				Console.CursorLeft = left;
+				Console.CursorTop = top;
+				Console.CursorVisible = visible;
+			} // ctor
+
+			public void Dispose()
+			{
+				Console.CursorLeft = OldCursorLeft;
+				Console.CursorTop = OldCursorTop;
+				Console.CursorVisible = OldVisible;
+			} // proc Dispose
+
+			public int OldCursorLeft { get; }
+			public int OldCursorTop { get; }
+			public bool OldVisible { get; }
+		} // class ResetCursor
+
+		public IDisposable SetCursor(int? left = null, int? top = null, bool? visible = null)
+			=> new ResetCursor(left ?? Console.CursorLeft, top ?? Console.CursorTop, visible ?? Console.CursorVisible);
+
+		#endregion
+
+		public object SyncRoot => screenLock;
 	} // class DebugView
 }
--- SimpleDbg/Program.cs
+++ SimpleDbg/Program.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using CommandLine;
+using Neo.IronLua;
+using TecWare.DE.Stuff;
 
 namespace TecWare.DE.Server
 {
+	#region -- class SimpleDebugArguments -----------------------------------------------
+
 	///////////////////////////////////////////////////////////////////////////////
 	/// <summary></summary>
 	public class SimpleDebugArguments
@@ -20,10 +27,39 @@ namespace TecWare.DE.Server
 		public int Wait { get; set; } = 0;
 	} // class SimpleDebugArguments
 
+	#endregion
+
+	#region -- class InteractiveCommandAttribute ----------------------------------------
+
+	///////////////////////////////////////////////////////////////////////////////
+	/// <summary></summary>
+	[AttributeUsage(AttributeTargets.Method)]
+	public class InteractiveCommandAttribute : Attribute
+	{
+		public InteractiveCommandAttribute(string name)
+		{
+			this.Name = name;
+		} // ctor
+
+		public string Name { get; }
+		public string Short { get; set; }
+		public string HelpText { get; set; }
+	} // class InteractiveCommandAttribute
+
+	#endregion
+
 	///////////////////////////////////////////////////////////////////////////////
 	/// <summary></summary>
 	public class Program
 	{
+		private static readonly DebugView view = new DebugView();
+		private static readonly Regex commandSyntax = new Regex(@"\:(?<cmd>\w+)(?:\s+(?<args>(?:\`[^\`]*\`)|(?:[^\s]*)))*", RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+		private static StringBuilder currentCommand = new StringBuilder(); // holds the current script
+		private static ClientDebugSession session;
+
+		#region -- Main, RunDebugProgram --------------------------------------------------
+
 		public static int Main(string[] args)
 		{
 			var parser = new Parser(
@@ -33,25 +69,21 @@ namespace TecWare.DE.Server
 					s.IgnoreUnknownArguments = false;
 				});
 
-			var  r =parser.ParseArguments<SimpleDebugArguments>(args);
+			var r = parser.ParseArguments<SimpleDebugArguments>(args);
 
 			return r.MapResult<SimpleDebugArguments, int>(
 				options =>
 				{
-					if (options.Wait > 0)
-						Thread.Sleep(options.Wait);
-
 					try
 					{
 						Task.Factory.StartNew(RunDebugProgram(options).Wait).Wait();
 					}
-					catch (AggregateException e)
-					{
-						Console.WriteLine(e.InnerException.Message);
-					}
 					catch (Exception e)
 					{
-						Console.WriteLine(e.Message);
+						view.WriteError(e);
+#if DEBUG
+						Console.ReadLine();
+#endif
 					}
 					return 0;
 				},
@@ -65,26 +97,207 @@ namespace TecWare.DE.Server
 
 		private static async Task RunDebugProgram(SimpleDebugArguments arguments)
 		{
-			var socket = new ClientWebSocket();
+			// simple wait
+			if (arguments.Wait > 0)
+				await Task.Delay(arguments.Wait);
 
 			// connection
-			
-			socket.Options.AddSubProtocol("dedbg");
-			await socket.ConnectAsync(new Uri(arguments.Uri), CancellationToken.None);
+			view.WriteLine("Connecting...");
+			session = new ClientDebugSession(new Uri(arguments.Uri));
+			await session.ConnectAsync();
 
 			// start request loop
-			while (socket.State == WebSocketState.Open)
+			while (true)
 			{
-				Console.Write("> ");
-				var cmd = Console.ReadLine();
-				await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(cmd)), WebSocketMessageType.Text, true, CancellationToken.None);
+				// read command
+				view.Write("> ");
+				var line = Console.ReadLine();
+
+				if (line.Length == 0) // nothing
+				{
+					if (currentCommand.Length == 0) // no current script
+						currentCommand.AppendLine();
+					else
+					{
+						await SendCommand(currentCommand.ToString());
+						currentCommand.Clear();
+					}
+				}
+				else if (line[0] == ':') // interactive command
+				{
+					var m = commandSyntax.Match(line);
+					if (m.Success)
+					{
+						// get the command
+						var cmd = m.Groups["cmd"].Value;
+
+						if (String.Compare(cmd, "q", StringComparison.OrdinalIgnoreCase) == 0 ||
+							String.Compare(cmd, "quit", StringComparison.OrdinalIgnoreCase) == 0)
+							break; // exit
+						else
+						{
+							var args = m.Groups["args"];
+							var argArray = new string[args.Captures.Count];
+							for (var i = 0; i < args.Captures.Count; i++)
+								argArray[i] = CleanArgument(args.Captures[i].Value ?? String.Empty);
+
+							try
+							{
+								RunCommand(cmd, argArray);
+							}
+							catch (Exception e)
+							{
+								view.WriteError(e);
+							}
+						}
+					}
+					else
+						view.WriteError("Command parse error."); // todo: error
+				}
+				else // add to command buffer
+				{
+					currentCommand.AppendLine(line);
+				}
+
+				//await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(cmd)), WebSocketMessageType.Text, true, CancellationToken.None);
+			}
+		} // proc RunDebugProgram
+
+		#endregion
+
+		#region -- RunCommand -------------------------------------------------------------
+
+		private static string CleanArgument(string value)
+			=> value.Length > 1 && value[0] == '`' && value[value.Length - 1] == '`' ? value.Substring(1, value.Length - 2) : value;
+
+		public static void RunCommand(string cmd, string[] argArray)
+		{
+			var ti = typeof(Program).GetTypeInfo();
+
+			var mi = (from c in ti.GetRuntimeMethods()
+								let attr = c.GetCustomAttribute<InteractiveCommandAttribute>()
+								where c.IsStatic && attr != null && (String.Compare(attr.Name, cmd, StringComparison.OrdinalIgnoreCase) == 0 || String.Compare(attr.Short, cmd, StringComparison.OrdinalIgnoreCase) == 0)
+								select c).FirstOrDefault();
+
+			if (mi == null)
+				throw new Exception($"Command '{cmd}' not found.");
+
+
+			var parameterInfo = mi.GetParameters();
+			var parameters = new object[parameterInfo.Length];
+
+			if (parameterInfo.Length > 0) // bind arguments
+			{
+				for (var i = 0; i < parameterInfo.Length; i++)
+				{
+					if (i < argArray.Length) // convert argument
+						parameters[i] = Procs.ChangeType(argArray[i], parameterInfo[i].ParameterType);
+					else
+						parameters[i] = parameterInfo[i].DefaultValue;
+				}
 			}
 
-			/*
-			 * Zwei modi: command
-			            > interactive
-		*/
+			// execute command
+			object r;
+			if (mi.ReturnType == typeof(Task)) // async
+			{
+				var t = (Task)mi.Invoke(null, parameters);
+				t.Wait();
+				r = null;
+			}
+			else
+				r = mi.Invoke(null, parameters);
 
-		} // proc RunDebugProgram
+			if (r != null)
+				view.WriteObject(r);
+		} // proc RunCommand
+
+		#endregion
+
+		#region -- ShowHelp ---------------------------------------------------------------
+
+		[InteractiveCommand("help", Short = "h", HelpText = "Shows this text.")]
+		private static void ShowHelp()
+		{
+			var ti = typeof(Program).GetTypeInfo();
+
+			view.WriteLine("Data Exchange Debugger");
+			view.WriteLine();
+
+			foreach (var cur in
+				from c in ti.GetRuntimeMethods()
+				let attr = c.GetCustomAttribute<InteractiveCommandAttribute>()
+				where c.IsStatic && attr != null
+				orderby attr.Name
+				select new Tuple<MethodInfo, InteractiveCommandAttribute>(c, attr))
+			{
+				view.Write("  ");
+				view.Write(cur.Item2.Name);
+				if (!String.IsNullOrEmpty(cur.Item2.Short))
+				{
+					view.Write(" [");
+					view.Write(cur.Item2.Short);
+					view.Write("]");
+				}
+				view.WriteLine();
+				view.Write("    ");
+				using (view.SetColor(ConsoleColor.DarkGray))
+					view.WriteLine(cur.Item2.HelpText);
+			}
+
+			view.WriteLine();
+		} // proc ShowHelp
+
+		#endregion
+
+		#region -- Clear, Quit ------------------------------------------------------------
+
+		[InteractiveCommand("clear", HelpText = "Clears the current command buffer.")]
+		private static void ClearCommandBuffer()
+		{
+			currentCommand.Clear();
+		} // proc ClearCommandBuffer
+
+		[InteractiveCommand("quit", Short = "q", HelpText = "Exit the application.")]
+		private static void DummyQuit() { }
+
+		#endregion
+
+		private static void WriteReturn(IEnumerable<ClientMemberValue> r)
+		{
+			foreach (var v in r)
+			{
+				lock (view.SyncRoot)
+				{
+					Console.Write(v.Name);
+					Console.Write(": ");
+					using (view.SetColor(ConsoleColor.DarkGray))
+					{
+						Console.Write("(");
+						Console.Write(v.TypeAsString);
+						Console.Write(")");
+					}
+					Console.WriteLine(v.ValueAsString);
+				}
+			}
+		} // proc WriteReturn
+
+		private static async Task SendCommand(string commandText)
+		{
+			WriteReturn(await session.SendExecuteAsync(commandText));
+		} // proc SendCommand
+
+		[InteractiveCommand("use", HelpText = "Activates a new global space, on which the commands are executed.")]
+		private static async Task SendUseNode(string node = null)
+		{
+			var p = await session.SendUseAsync(node ?? String.Empty);
+			view.WriteLine($"Current Node: {p}");
+		} // func SendUseNode
+
+		[InteractiveCommand("members", Short = "m", HelpText = "Lists the current available global variables.")]
+		private static async Task SendVariables()
+		{
+			WriteReturn(await session.SendMembersAsync(String.Empty));
+		} // proc SendVariables
 	} // class Program
 }
