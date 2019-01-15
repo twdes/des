318c1ec95934a1f773b000435834b20115794c1f
neolithos <neolithos@outlook.com>
2019-01-15 22:10:24 +0100

    Chg: project format to net standard

--- ServerLib/Properties/AssemblyInfo.cs
+++ ServerLib/Properties/AssemblyInfo.cs
@@ -1,10 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Data Exchange Server Library")]
-[assembly: AssemblyDescription("Interfaces, contracts for the common functionality of the server.")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7b19d82f-1684-4778-ac8e-940af0521702")]
