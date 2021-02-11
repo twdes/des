22488cef4ea24873a2255a0fa2e72d6c385b19b6
Praktikant <Praktikant@APFEL>
2021-02-11 14:34:59 +0100

    Tests für CSV-Writer

--- ServerTests/CsvTest.cs
+++ ServerTests/CsvTest.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TecWare.DE.Data;
+using TecWare.DE.Stuff;
 
 namespace TecWare.DE.Server
 {
@@ -117,15 +118,15 @@ namespace TecWare.DE.Server
 		// TestMethodes for TextFixedWriter
 		[TestMethod]
 		public void TryTextFixedWriter01()
-        {
+		{
 			using (var sw = new StringWriter())
-            {
-				using(var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5 } }))
-                {
+			{
+				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5 } }))
+				{
 					w.WriteRow(new string[] { "hi" });
 				}
 				Assert.AreEqual("hi   \r\n", sw.ToString());
-            }
+			}
 		}
 
 		[TestMethod]
@@ -159,11 +160,11 @@ namespace TecWare.DE.Server
 		{
 			using (var sw = new StringWriter())
 			{
-				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5,5 } }))
+				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5, 5 } }))
 				{
 					w.WriteRow(new string[] { "Wert1", "Wert2" });
 				}
-				Assert.AreEqual("Wert1\r\nWert2\r\n", sw.ToString());
+				Assert.AreEqual("Wert1Wert2\r\n", sw.ToString());
 			}
 		}
 
@@ -174,7 +175,7 @@ namespace TecWare.DE.Server
 			{
 				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 12, 5 } }))
 				{
-					w.WriteRow(new string[] { "zuwenig Rows"});
+					w.WriteRow(new string[] { "zuwenig Rows" });
 				}
 				Assert.AreEqual("zuwenig Rows\r\n", sw.ToString());
 			}
@@ -185,11 +186,25 @@ namespace TecWare.DE.Server
 		{
 			using (var sw = new StringWriter())
 			{
-				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5, 5 } }))
+				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5, 5 }}))
 				{
-					w.WriteRow(new string[] { "zu" , "viele", "Rows" });
+					w.WriteRow(new string[] { "zu", "viele", "Rows" });
 				}
 				Assert.AreEqual("zu   viele\r\n", sw.ToString());
+			}
+		}
+
+		[TestMethod]
+		public void TryTextFixedWriter07()
+		{
+			// HeaderRow > StartRow
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextFixedWriter(sw, new TextFixedSettings() { Lengths = new int[] { 5 }, HeaderRow = 2, StartRow= 1 }))
+				{
+					w.WriteRow(new string[] { "hi" });
+				}
+				Assert.AreEqual("hi   \r\n", sw.ToString());
 			}
 		}
 
@@ -197,33 +212,33 @@ namespace TecWare.DE.Server
 		//Tests for TextCsvWriter
 
 		public void TestTextCsvWriter(string expectedValue, TextCsvWriter actualValue)
-        {
+		{
 			Assert.AreEqual(expectedValue, actualValue.BaseWriter.ToString());
-        }
+		}
 
-
+		//Forced
 		[TestMethod]
 		public void TryTextCsvWriter01()
-        {
-			using( var sw = new StringWriter())
-            {
+		{
+			using (var sw = new StringWriter())
+			{
 				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Forced, Quote = '\"', Delemiter = ',' }))
-                {
-					w.WriteRow(new string[] { "Test "});
+				{
+					w.WriteRow(new string[] { "Test " });
 					Assert.AreEqual("\"Test \"\r\n", w.BaseWriter.ToString());
-                }
-            }
-        }
+				}
+			}
+		}
 
 		[TestMethod]
 		public void TryTextCsvWriter02()
 		{
 			using (var sw = new StringWriter())
 			{
-				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.ForceText, Quote = '\"', Delemiter = ',' ,HeaderRow = 0 }))
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Forced, Quote = '\"', Delemiter = ',', HeaderRow = 0 }))
 				{
-					w.WriteRow(new string[] { "\"ForcedText\" ", "123" });
-					Assert.AreEqual("\"ForcedText\" ,123\r\n", w.BaseWriter.ToString());
+					w.WriteRow(new string[] { "\"Quote\"", "SimpleText" });
+					Assert.AreEqual("\"\"\"Quote\"\"\",\"SimpleText\"\r\n", w.BaseWriter.ToString());
 				}
 			}
 		}
@@ -233,16 +248,99 @@ namespace TecWare.DE.Server
 		{
 			using (var sw = new StringWriter())
 			{
-				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.None, Quote = '\"', Delemiter = ',' }))
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Forced, Quote = '\"', Delemiter = ',', HeaderRow = 0 }))
 				{
-					w.WriteRow(new string[] { "NoQuote " });
-					Assert.AreEqual("NoQuote \r\n", w.BaseWriter.ToString());
+					w.WriteRow(new string[] { "\"\"", "" });
+					Assert.AreEqual("\"\"\"\"\"\",\"\"\r\n", w.BaseWriter.ToString());
 				}
 			}
 		}
 
 		[TestMethod]
 		public void TryTextCsvWriter04()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Forced, Quote = '\"', Delemiter = ',', HeaderRow = 0 }))
+				{
+					w.WriteRow(new string[] { "\"This is a \"Quote\"\"" });
+					Assert.AreEqual("\"\"\"This ist a \"Quote\"\"\"\"\"\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+		// ForceText
+		[TestMethod]
+		public void TryTextCsvWriter05()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.ForceText, Quote = '\'', Delemiter = ',', HeaderRow = 0 }))
+				{
+					w.WriteRow(new string[] { "\'ForcedText\' ", "123" });
+					Assert.AreEqual("\'ForcedText\' ,123\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryTextCsvWriter06()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.ForceText, Quote = '\"', Delemiter = ',', HeaderRow = 0 }))
+				{
+					w.WriteRow(new string[] { "\"Forced\"Text\"\" ", "123" });
+					Assert.AreEqual("\"Forced\"Text\"\" ,123\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryTextCsvWriter07()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.ForceText, Quote = '\"', Delemiter = ',', HeaderRow = 0 }))
+				{
+					w.WriteRow(new string[] { "\"This is a \"Quote\"\"" });
+					Assert.AreEqual("\"This is a \"Quote\"\"\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+
+		//None
+		[TestMethod]
+		public void TryTextCsvWriter08()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.None, Quote = '\"', Delemiter = ',' }))
+				{
+					w.WriteRow(new string[] { "NoQuote", "NoQuote2" });
+					Assert.AreEqual("NoQuote,NoQuote2\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryTextCsvWriter09()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.None, Quote = '\'', Delemiter = ',' }))
+				{
+					w.WriteRow(new string[] { "No \'Quote\'" });
+					Assert.AreEqual("No \'Quote\'\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+
+		//Normal
+		[TestMethod]
+		public void TryTextCsvWriter10()
 		{
 			using (var sw = new StringWriter())
 			{
@@ -254,35 +352,362 @@ namespace TecWare.DE.Server
 			}
 		}
 
+		[TestMethod]
+		public void TryTextCsvWriter11()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Normal, Quote = '\"', Delemiter = ',' }))
+				{
+					w.WriteRow(new string[] { "", "\"\"" });
+					Assert.AreEqual(",\"\"\"\"\"\"\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryTextCsvWriter12()
+		{
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextCsvWriter(sw, new TextCsvSettings() { Quotation = CsvQuotation.Forced, Quote = '\"', Delemiter = ',' }))
+				{
+					w.WriteRow(new string[] {null});
+					Assert.AreEqual("\"\"\r\n", w.BaseWriter.ToString());
+				}
+			}
+		}
+
+
+
 		// Tests für TextDataRowWriter
+
+
+
+		private static readonly IDataColumn[] col = new IDataColumn[]
+		{
+			new SimpleDataColumn ("String", typeof(string)),
+			new SimpleDataColumn ("Int", typeof(Int32)),
+		};
+
+		private static readonly IDataColumn[] col2 = new IDataColumn[]
+		{
+			new SimpleDataColumn ("\"String\"", typeof(string)),
+			new SimpleDataColumn ("!§$%&/()=*+'#><|", typeof(Int32)),
+		};
+
+		private static readonly IDataColumn[] col3 = new IDataColumn[]
+		{
+		};
+
+
 
 		[TestMethod]
 		public void TryDataRowWriter01()
-        {
+		{
 
 			using (var sw = new StringWriter())
-            {
-				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, new TextDataRowColumn {Name = "Col1", DataType = typeof(string) } ))
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col))
                 {
-					w.CoreWriter.WriteRow(new string[] { "Col1", "Col2", "Col3" });
-					Assert.AreEqual("test", sw.ToString());
-                }
-            }
-        }
-
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col),
+							new SimpleDataRow(new object[]{ "value3", 3 }, col)
+						}
+					);
+					
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						"\"value1\";1\r\n" +
+						"\"value2\";2\r\n" +
+						"\"value3\";3\r\n", text);
+				}
+			}
+		}
 
 		[TestMethod]
 		public void TryDataRowWriter02()
-        {
+		{
+			// mehr Felder als Columns
 			using (var sw = new StringWriter())
-            {
-				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1, Delemiter= ',' },  new TextDataRowColumn { Name = "Col1", DataType = typeof(string) }))
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col))
 				{
-					w.CoreWriter.WriteRow(new string[] {"Col1", "Col2", "Col3"});
-					Assert.AreEqual("Col1,Col2,Col3", sw.ToString());
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1, "extraValue" }, col),
+							new SimpleDataRow(new object[]{ "value2", 2, "extraValue" }, col),
+							new SimpleDataRow(new object[]{ "value3", 3, "extraValue" }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						"value1;1\r\n" +
+						"value2;2\r\n" +
+						"value3;3\r\n", text);
 				}
 			}
-
 		}
+
+		[TestMethod]
+		public void TryDataRowWriter03()
+		{
+			//null-values
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ null, 1}, col),
+							new SimpleDataRow(new object[]{ "value2", null}, col),
+							new SimpleDataRow(new object[]{ null, null}, col),
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						";1\r\n" +
+						"value2;\r\n" +
+						";\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter04()
+		{
+			//wrong Datatype
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1, Quotation= CsvQuotation.None }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ 1, 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", "intvalue" }, col),
+							new SimpleDataRow(new object[]{ "value3", "3" }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("\"String\";Int\r\n" +
+						"1;1\r\n" +
+						"value2;intvalue\r\n" +
+						"value3;3\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter05()
+		{
+			//Sonderzeichen
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col2))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "äüö", -1 }, col2),
+							new SimpleDataRow(new object[]{ "@><", 2 }, col2),
+							new SimpleDataRow(new object[]{ "*'#", 3 }, col2),
+							new SimpleDataRow(new object[]{ "!§$%&()=?²", 42 }, col2)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;!§$%&/()=*+'#><|\r\n" +
+						"äüö;-1\r\n" +
+						"@><;2\r\n" +
+						"*'#;3\r\n" +
+						"!§$%&()=?²;42\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter06()
+		{
+			//mit TextFixedSettings
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextFixedSettings() { Padding = ' ', Lengths = new int[] {10,3 }, HeaderRow= 1 }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 123 }, col),
+							new SimpleDataRow(new object[]{ "value2", 234 }, col),
+							new SimpleDataRow(new object[]{ "value3", 345 }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String    Int\r\n" +
+						"value1  123\r\n" +
+						"value2  234\r\n" +
+						"\"value3\"  345\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter07()
+		{
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1, Quotation= CsvQuotation.Forced }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col),
+							new SimpleDataRow(new object[]{ "value3", 3 }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("\"String\";\"Int\"\r\n" +
+						"\"value1\";\"1\"\r\n" +
+						"\"value2\";\"2\"\r\n" +
+						"\"value3\";\"3\"\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter08()
+		{
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1, Quotation = CsvQuotation.ForceText }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col),
+							new SimpleDataRow(new object[]{ "value3", 3 }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						"\"value1\";1\r\n" +
+						"\"value2\";2\r\n" +
+						"\"value3\";3\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter09()
+		{
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1, Quotation = CsvQuotation.None }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col),
+							new SimpleDataRow(new object[]{ "value3", 3 }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						"value1;1\r\n" +
+						"value2;2\r\n" +
+						"value3;3\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter10()
+		{
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter11()
+		{
+
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 0, StartRow = 1 }, col3))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col3),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col3),
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("\r\n\r\n\r\n", text);
+				}
+			}
+		}
+
+		[TestMethod]
+		public void TryDataRowWriter12()
+		{
+			// HeaderRow > StartRow
+			using (var sw = new StringWriter())
+			{
+				using (var w = new TextDataRowWriter(sw, new TextCsvSettings() { HeaderRow = 2, StartRow = 0 }, col))
+				{
+					w.Write(
+						new IDataRow[]
+						{
+							new SimpleDataRow(new object[]{ "value1", 1 }, col),
+							new SimpleDataRow(new object[]{ "value2", 2 }, col),
+							new SimpleDataRow(new object[]{ "value3", 3 }, col)
+						}
+					);
+
+					var text = sw.ToString();
+					Assert.AreEqual("String;Int\r\n" +
+						"value1;1\r\n" +
+						"value2;2\r\n" +
+						"value3;3\r\n", text);
+				}
+			}
+		}
+
+
 	}
 }
